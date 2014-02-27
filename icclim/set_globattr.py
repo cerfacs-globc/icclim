b9f9ff34ba8be6f00d589094c019d7b4ea6bc41b
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-02-27 10:47:15 +0100

    set_history_globattr changed

--- icclim/set_globattr.py
+++ icclim/set_globattr.py
@@ -1,3 +1,5 @@
+# set the global attributs "title" and "history" in output meta data
+
 
 def set_title_globattr(out_nc, indice_group, indice_name):
     '''
@@ -12,7 +14,7 @@ def set_title_globattr(out_nc, indice_group, indice_name):
     
     '''
     
-    # example: title: ECA heat indice SU
+    # example:      title: ECA heat indice SU
     out_nc.setncattr('title', 'ECA '+ indice_group + ' indice ' + indice_name)
     
 def set_history_globattr(out_nc, calc_grouping, indice_name, time_range):
@@ -20,7 +22,7 @@ def set_history_globattr(out_nc, calc_grouping, indice_name, time_range):
     Set the global attribute "title" in output meta data
     
     :param out_nc: out NetCDF dataset
-    :type out: netCDF4.Dataset
+    :type out_nc: netCDF4.Dataset
     :param calc_grouping: temporal grouping to apply for calculations
     :type calc_grouping: list of str or int
     :param indice_name: name of indice 
@@ -31,20 +33,19 @@ def set_history_globattr(out_nc, calc_grouping, indice_name, time_range):
     
     '''
     
-    ## example: title: ECA heat indice SU
-    #out_nc.setncattr('title', 'ECA '+ indice_group + ' indice ' + indice_name)
-    #
-    #
-    #def setglobattr_history(onc, indice_name, slice_mode, dt1, dt2):
-    #
-    #current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    #
-    #if slice_mode == 'year':
-    #    mode = 'annual'
-    #if slice_mode == 'month':
-    #    mode = 'monthly'
-    #
-    ## example of history_str_value: 2012-10-02 15:30:20 Calculation of SU indice (annual) from 1960-01-01 to 1990-12-31.
-    #history_str_value = current_time + ' Calculation of ' + indice_name + ' indice (' + mode + ') from ' + dt1.strftime('%Y-%m-%d') + ' to ' + dt2.strftime('%Y-%m-%d') + '.'
-    #
-    #onc.setncattr('history', history_str_value) 
+    
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    dt1 = time_range[0]
+    dt2 = time_range[1]
+    
+    if calc_grouping == ['year','month']:
+        mode = 'monthly'
+    elif calc_grouping == ['year']:
+        mode = 'annual'
+        
+    # etc ...
+    
+    # example of history_str_value:     2012-10-02 15:30:20 Calculation of SU indice (monthly) from 1960-01-01 to 1990-12-31.
+    history_str_value = current_time + ' Calculation of ' + indice_name + ' indice (' + mode + ') from ' + dt1.strftime('%Y-%m-%d') + ' to ' + dt2.strftime('%Y-%m-%d') + '.'
+    
+    out_nc.setncattr('history', history_str_value) 
