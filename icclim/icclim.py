2616cbfea2f997862dd9a9de4986b8ce610fb9c8
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-03-03 12:18:15 +0100

    Added GSL indice...

--- icclim/icclim.py
+++ icclim/icclim.py
@@ -792,137 +792,165 @@ def GD4_calculation(a, fill_val):
     return indice
 
 
-## the GSL indice calculation is very slow => I will optimize with C #####
-################################################################## GSL #################################
+### the GSL indice calculation is very slow => I will optimize with C #####
+################################################################### GSL #################################
+#
+#def find_first_index_consecutive(a, min_len, threshold, operation):
+#    '''
+#    find the first index of a consecutive sequence (for the given condition)
+#    in 1D numpy array
+#    
+#    : a                     1D numpy array
+#    : min_len               minimum length of the consecutive sequence
+#    : threshold             threshold value for a condition (units must be the same as in array)
+#    : operation             logical operation ('e', 'gt', 'get', 'lt', 'let')
+#    
+#    : return                first index of a consecutive sequence
+#    '''
+#
+#    first_ind = -1    
+#    
+#    if operation == 'e': # >
+#        for i in range(len(a)-min_len+1):
+#            if (a[i:i+min_len] == threshold).all() :
+#                first_ind = i
+#                break
+#       
+#    if operation == 'gt': # >
+#        for i in range(len(a)-min_len+1):
+#            if (a[i:i+min_len] > threshold).all() :
+#                first_ind = i
+#                break
+#    
+#    elif operation == 'get': # >=
+#        for i in range(len(a)-min_len+1):
+#            if (a[i:i+min_len] >= threshold).all() :
+#                first_ind = i
+#                break
+#
+#    elif operation == 'lt': # <
+#        for i in range(len(a)-min_len+1):
+#            if (a[i:i+min_len] < threshold).all() :
+#                first_ind = i
+#                break
+#        
+#    elif operation == 'let': # <
+#        for i in range(len(a)-min_len+1):
+#            if (a[i:i+min_len] <= threshold).all() :
+#                first_ind = i
+#                break    
+#
+#    return first_ind
+#
+#def count_nb_days(time_arr, index_from, index_to):
+#    '''
+#    count number of days between time_arr[index_from] and time_arr[index_to] 
+#    
+#    '''
+#    dt_from = time_arr[index_from]
+#    dt_to = time_arr[index_to]
+#    
+#    nb_days = (dt_to - dt_from).days
+#    
+#    return nb_days
+#
+#def GSL_point(a, time_arr, fill_val, t, ndays):
+#    '''
+#    growing season length (days) for one year
+#    
+#    : a                     1D numpy array of floats (temperature in Kelvin)
+#    : time_arr              1D numpy array with datetime.datetime objects (must be for one year)
+#    : t                     temperature threshold (degree Celsius, default: t = 5 Celsius)                        
+#                            len(a)=len(time_arr)
+#    : ndays                 number of consecutif days (default: ndays = 6)
+#    '''
+#    
+#    time_arr.sort()
+#    
+#    # we want to find index of the first day of a consecutive sequence (>=6 days) where tempereture > 5 Celsius degrees  
+#    index_from = find_first_index_consecutive(a, ndays, t+273.15, 'gt') # threshold = 5C + 273.15 [Kelvin]
+#    #print index_from
+#    
+#    
+#    # we want to find index of the first day of a consecutive sequence (>=6 days) where tempereture < 5 Celsius degrees
+#    # after the 1st July
+#    
+#    current_year = time_arr[0].year # we get the year
+#    time_arr2 = time_arr[ time_arr>=datetime(current_year, 07,1) ] # time_arr2 must be from 1st July
+#    
+#    # we want to find index (where time = 1st July) for subsetting of a
+#    ind = len(time_arr) - len(time_arr2)
+#    
+#    # subsetting of a from ind: a = ([x x x x x ind x x x x])
+#    a2 = a[ind:] # a2 = ([ind x x x x]) 
+#
+#    # index of the first day of a consecutive sequence (>=6 days) where tempereture < 5 Celsius degrees
+#    # in a2 
+#    index_a2 = find_first_index_consecutive(a2, ndays, t+273.15, 'lt') # if a2 == [] -> index_a2 = -1
+#    
+#    # index of the first day of a consecutive sequence (>=6 days) where tempereture < 5 Celsius degrees
+#    # in a
+#    if index_a2 == -1 or index_from == -1:
+#        GSL = fill_val
+#    
+#    else:    
+#        index_to = len(a) - len(a2) + index_a2
+#        #print index_to
+#
+#        GSL = count_nb_days(time_arr, index_from, index_to)
+#    
+#    return GSL
+#
+#
+#
+#def GSL_calculation(a, time_arr, fill_val, t=5, ndays=6):
+#    '''
+#    : a                 3d array (here: tas mean)
+#    : fill_val          fill value (usually: 1.0e+20)
+#    : t                 temperature threshold (degree Celsius, default: t = 5 Celsius)
+#    : ndays             number of consecutif days (default: ndays = 6)
+#    
+#    : return            2d array
+#    '''
+#    
+#    T = t + 273.15 # Celsius -> Kelvin
+#    
+#    indice = numpy.empty(shape = (a.shape[1],a.shape[2]))
+#
+#    for i in range(indice.shape[0]):
+#        for j in range(indice.shape[1]):
+#            indice[i,j] = GSL_point(a[:,i,j], time_arr, fill_val, t, ndays)
+#    return indice
+#
+######################################################################## end GSL ##################################
 
-def find_first_index_consecutive(a, min_len, threshold, operation):
+def GSL_calculation(a, fill_val):
+
     '''
-    find the first index of a consecutive sequence (for the given condition)
-    in 1D numpy array
-    
-    : a                     1D numpy array
-    : min_len               minimum length of the consecutive sequence
-    : threshold             threshold value for a condition (units must be the same as in array)
-    : operation             logical operation ('e', 'gt', 'get', 'lt', 'let')
-    
-    : return                first index of a consecutive sequence
+    Calculates the GSL indice: ...
     '''
 
-    first_ind = -1    
-    
-    if operation == 'e': # >
-        for i in range(len(a)-min_len+1):
-            if (a[i:i+min_len] == threshold).all() :
-                first_ind = i
-                break
-       
-    if operation == 'gt': # >
-        for i in range(len(a)-min_len+1):
-            if (a[i:i+min_len] > threshold).all() :
-                first_ind = i
-                break
-    
-    elif operation == 'get': # >=
-        for i in range(len(a)-min_len+1):
-            if (a[i:i+min_len] >= threshold).all() :
-                first_ind = i
-                break
-
-    elif operation == 'lt': # <
-        for i in range(len(a)-min_len+1):
-            if (a[i:i+min_len] < threshold).all() :
-                first_ind = i
-                break
-        
-    elif operation == 'let': # <
-        for i in range(len(a)-min_len+1):
-            if (a[i:i+min_len] <= threshold).all() :
-                first_ind = i
-                break    
-
-    return first_ind
-
-def count_nb_days(time_arr, index_from, index_to):
-    '''
-    count number of days between time_arr[index_from] and time_arr[index_to] 
-    
-    '''
-    dt_from = time_arr[index_from]
-    dt_to = time_arr[index_to]
-    
-    nb_days = (dt_to - dt_from).days
-    
-    return nb_days
-
-def GSL_point(a, time_arr, fill_val, t, ndays):
-    '''
-    growing season length (days) for one year
-    
-    : a                     1D numpy array of floats (temperature in Kelvin)
-    : time_arr              1D numpy array with datetime.datetime objects (must be for one year)
-    : t                     temperature threshold (degree Celsius, default: t = 5 Celsius)                        
-                            len(a)=len(time_arr)
-    : ndays                 number of consecutif days (default: ndays = 6)
-    '''
-    
-    time_arr.sort()
-    
-    # we want to find index of the first day of a consecutive sequence (>=6 days) where tempereture > 5 Celsius degrees  
-    index_from = find_first_index_consecutive(a, ndays, t+273.15, 'gt') # threshold = 5C + 273.15 [Kelvin]
-    #print index_from
-    
-    
-    # we want to find index of the first day of a consecutive sequence (>=6 days) where tempereture < 5 Celsius degrees
-    # after the 1st July
-    
-    current_year = time_arr[0].year # we get the year
-    time_arr2 = time_arr[ time_arr>=datetime(current_year, 07,1) ] # time_arr2 must be from 1st July
-    
-    # we want to find index (where time = 1st July) for subsetting of a
-    ind = len(time_arr) - len(time_arr2)
-    
-    # subsetting of a from ind: a = ([x x x x x ind x x x x])
-    a2 = a[ind:] # a2 = ([ind x x x x]) 
-
-    # index of the first day of a consecutive sequence (>=6 days) where tempereture < 5 Celsius degrees
-    # in a2 
-    index_a2 = find_first_index_consecutive(a2, ndays, t+273.15, 'lt') # if a2 == [] -> index_a2 = -1
-    
-    # index of the first day of a consecutive sequence (>=6 days) where tempereture < 5 Celsius degrees
-    # in a
-    if index_a2 == -1 or index_from == -1:
-        GSL = fill_val
-    
-    else:    
-        index_to = len(a) - len(a2) + index_a2
-        #print index_to
-
-        GSL = count_nb_days(time_arr, index_from, index_to)
-    
-    return GSL
-
-
-
-def GSL_calculation(a, time_arr, fill_val, t=5, ndays=6):
-    '''
-    : a                 3d array (here: tas mean)
-    : fill_val          fill value (usually: 1.0e+20)
-    : t                 temperature threshold (degree Celsius, default: t = 5 Celsius)
-    : ndays             number of consecutif days (default: ndays = 6)
-    
-    : return            2d array
-    '''
-    
+    t = 5 # temperature threshold (degree Celsius)
     T = t + 273.15 # Celsius -> Kelvin
+    indexMiddleOfYear=181 # this should be calculated, this value is the normal value when size of time vector is 365. In this case, it corresponds to the 1st of July
     
-    indice = numpy.empty(shape = (a.shape[1],a.shape[2]))
+    C_find_GSL_3d = libraryC.find_GSL_3d
+    C_find_GSL_3d.restype = None
+    C_find_GSL_3d.argtypes = [ndpointer(ctypes.c_float),
+                                                    ctypes.c_int,
+                                                    ctypes.c_int,
+                                                    ctypes.c_int,
+                                                    ndpointer(ctypes.c_double),
+                                                    ctypes.c_float,
+                                                    ctypes.c_float,
+                                                    ctypes.c_int]
+    
+    indice = numpy.zeros([a.shape[1],a.shape[2]]) # reserve memory
+    
+    C_find_GSL_3d(a, a.shape[0], a.shape[1], a.shape[2], indice, T, fill_val, indexMiddleOfYear)
+    indice = indice.reshape(a.shape[1],a.shape[2])
 
-    for i in range(indice.shape[0]):
-        for j in range(indice.shape[1]):
-            indice[i,j] = GSL_point(a[:,i,j], time_arr, fill_val, t, ndays)
-    return indice
-
-####################################################################### end GSL ##################################
+    return indice 
    
 ###### rain indices
 def RR_calculation(prr_arr, fill_val):
--- icclim/libC.c
+++ icclim/libC.c
@@ -162,7 +162,7 @@ int find_max_len_consec_sequence_1d(const float *indata,int i, int j, float thre
     }
 
 
-// fonction appelee depus python in CSU_arr_C
+// fonction appelee depus python 
 void find_max_len_consec_sequence_3d(const float *indata, int _sizeT,int _sizeI,int _sizeJ, double *outdata, float temp, float fill_val, char *operation) 
     {
     // find max length of a consecutif sequence in 3D array (along time axis) in a logical condition
@@ -181,6 +181,100 @@ void find_max_len_consec_sequence_3d(const float *indata, int _sizeT,int _sizeI,
                 }
     }
     
+
+
+// indexMiddleOfYear is the index of the 1st day in the 2nd semester for which we have a value for temperature
+// -> in a complete year, it should be the 1st of july : indexMiddleOfYear = 181 (or 182 in a bissextile year)
+int find_GSL_1d(const float *indata,int i, int j, float thresh, float fill_val, int indexMiddleOfYear)
+{
+        float previous=-999;
+        int nb=0;
+        int t;
+        int sequenceLength=6;
+        int T1 = -1;
+        int T2 = -1;
+        int returnValue;
+        
+        // - search T1 : beginning of 1st sequence of at least 6 consecutive days with temp > thresh
+        // - the search is done from 1st day of year to the last day of year
+        for (t = 0; t < sizeT; t++)
+            {
+                float val = getElementAt(indata,t,i,j);
+
+                if ((val>thresh) && (val != fill_val))
+                    {
+                        if (previous>thresh) nb++;
+                        else nb=1;	
+                    }
+                else nb=0;
+                
+                previous=val;
+                
+                if (nb == sequenceLength) { // found T1
+                    T1 = t-sequenceLength+1;
+                    break; // if we found at least 6 consecutive days with temp > thresh, we exit from the for loop
+                }
+                
+            }
+        
+        // - search T2 : beginning of 1st sequence of at least 6 consecutive days with temp < thresh
+        // - the search is done from the index indexMiddleOfYear to the last day of year.
+        // - indexMiddleOfYear should usually be the index of 1st of July, i.e 181 in a normal year
+        previous=999;
+        nb=0;
+        for (t = indexMiddleOfYear; t < sizeT; t++)
+            {
+                float val = getElementAt(indata,t,i,j);
+
+                if ((val<thresh) && (val != fill_val))
+                    {
+                        if (previous<thresh) nb++;
+                        else nb=1;	
+                    }
+                else nb=0;
+                
+                previous=val;
+                
+                if (nb == sequenceLength) { // found T2
+                    T2 = t-sequenceLength+1;
+                    break; // if we found at least 6 consecutive days with temp < thresh, we exit from the for loop
+                }
+                
+            }
+        
+        
+        // calculate the return value of this function
+        if (T1!=-1 && T2!=-1 && T1<T2){ // T1 and T2 were found and T1<T2 : we can calculate a result for find_GSL_1d
+            returnValue = T2-T1;
+        }
+        else { // impossible to return a result for find_GSL_1d, so we return fill_val
+            returnValue = fill_val;
+        }
+        
+        return returnValue;
+    
+}
+
+
+
+
+void find_GSL_3d(const float *indata, int _sizeT,int _sizeI,int _sizeJ, double *outdata, float temp, float fill_val, int indexMiddleOfYear)
+    {
+    
+        setGlobalVariables(_sizeT,_sizeI,_sizeJ);
+        int i,j;
+        
+            for (i = 0; i < sizeI; i++)
+                {
+                    for (j = 0; j < sizeJ; j++)
+                        {
+                            outdata[i*sizeJ+j] = find_GSL_1d(indata,i,j,temp, fill_val, indexMiddleOfYear);
+                        }
+                }
+    }
+ 
+
+
 
 
     
