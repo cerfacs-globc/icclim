8f4b5f1bc816c8cedd1a19b2c5b6289678e38495
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-04-30 16:11:55 +0200

    test...

--- icclim/icclim_doc/source/conf.py
+++ icclim/icclim_doc/source/conf.py
@@ -105,7 +105,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #html_theme = 'default'
 if on_rtd:
-    #html_theme = 'default'
+    html_theme = 'default'
     html_style = '/default.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
