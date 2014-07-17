ade44b4e667b69df81894a999d50bbaee2d92e3d
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-07-17 17:49:47 +0200

    Doc update

--- icclim/icclim_doc/source/conf.py
+++ icclim/icclim_doc/source/conf.py
@@ -296,6 +296,6 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'ctypes', 'numpy.ctypeslib', 'ctypes.cdll', 'calc_indice']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'ctypes', 'numpy.ctypeslib', 'ctypes.cdll', 'calc_indice', 'calc_indice_perc', 'percentile_dict']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
