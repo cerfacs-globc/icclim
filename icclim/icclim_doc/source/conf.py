d9bb57c125e954e74cf44ac496f4bcd1b7e3c1b0
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-07-17 16:08:40 +0200

    Doc update

--- icclim/icclim_doc/source/conf.py
+++ icclim/icclim_doc/source/conf.py
@@ -20,7 +20,8 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('/home/globc/tatarinova/codes/LIBTEST/icclim_github/icclim_p/icclim/'))
+#sys.path.insert(0, os.path.abspath('/home/globc/tatarinova/codes/LIBTEST/icclim_github/icclim_p/icclim/'))
+sys.path.insert(0, os.path.abspath('..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
