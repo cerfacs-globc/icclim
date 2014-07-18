2fab4325a83311b73ca7ebac811305f3eaabf5e2
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-07-18 09:26:08 +0200

    ...

--- icclim/icclim_doc/source/conf.py
+++ icclim/icclim_doc/source/conf.py
@@ -296,6 +296,6 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'ctypes', 'numpy.ctypeslib', 'ctypes.cdll']
+MOCK_MODULES = ['numpy', 'scipy', 'ctypes', 'numpy.ctypeslib', 'ctypes.cdll', 'libC', 'libC.c', 'libC.so']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
