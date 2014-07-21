7bc5bbc18e8e8f6c3a29b11afd9fab2368a3ae43
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-07-21 14:02:11 +0200

    updated by Maarten

--- icclim/set_longname_units_custom_indices.py
+++ icclim/set_longname_units_custom_indices.py
@@ -1,16 +1,32 @@
 # set the attributs "long_name" and "units" of indice variable in output meta data
 # (for indices with user defined thrshold)
 
+
+def SU_setthresholdattr(threshold_var):
+    threshold_var.setncattr('long_name', "Threshold temperature in degrees Celsius")
+    threshold_var.setncattr('standard_name', "air_temperature")
+    threshold_var.setncattr('units', "degrees Celsius")
+
 def SU_setvarattr(var_nc, threshold):
     long_name_str = 'Number of days with daily maximum temperature > {0} degrees)'.format(threshold)
     var_nc.setncattr('long_name', long_name_str)
     var_nc.setncattr('units', 'days')
 
+def CSU_setthresholdattr(threshold_var):
+    threshold_var.setncattr('long_name', "Threshold temperature in degrees Celsius")
+    threshold_var.setncattr('standard_name', "air_temperature")
+    threshold_var.setncattr('units', "degrees Celsius")
+    
 def CSU_setvarattr(var_nc, threshold):
     long_name_str = 'Maximum number of consecutive days with daily maximum temperature > {0} degrees)'.format(threshold)
     var_nc.setncattr('long_name', long_name_str)
     var_nc.setncattr('units', 'days')
 
+def TR_setthresholdattr(threshold_var):
+    threshold_var.setncattr('long_name', "Threshold temperature in degrees Celsius")
+    threshold_var.setncattr('standard_name', "air_temperature")
+    threshold_var.setncattr('units', "degrees Celsius")
+    
 def TR_setvarattr(var_nc, threshold):
     long_name_str = 'Number of days with daily minimum temperature > {0} degrees'.format(threshold)
     var_nc.setncattr('long_name', long_name_str)
