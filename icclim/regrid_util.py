3a9b7ca18b93c0fd60c35d686cca88e55d1e82e3
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-08-20 14:47:36 +0200

    regrid...

--- icclim/regrid_util.py
+++ icclim/regrid_util.py
@@ -6,6 +6,7 @@ from netCDF4 import Dataset
 from os.path import basename
 import icclim
 
+#esmpy = ESMF.Manager(logkind=ESMF.LogKind.MULTI, debug=True)
 
 def get_regrided_var(f_src, f_dst, varname):
     '''
@@ -314,3 +315,46 @@ def get_dst_src_files(file_list, resolution_type=1):
 >>> print file_list_final
 
 '''
+
+
+f1 = '/data/tatarinova/tasmax_day_EC-EARTH_rcp26_r8i1p1_2077.nc'                                    # 160 x 320
+f2 = '/data/tatarinova/CMIP5/tasmax_day/tasmax_day_CNRM-CM5_historical_r1i1p1_18550101-18591231.nc' # 128 x 256
+f3 = '/data/tatarinova/CMIP5/tasmax_day/tasmax_day_CNRM-CM5_historical_r1i1p1_20050101-20051231.nc' # 128 x 256
+f4 = '/data/tatarinova/tasmax_day_EC-EARTH_rcp26_r8i1p1_20770401-20770410.nc'                       # 160 x 320
+f5 = '/home/globc/tatarinova/Downloads/tasmax_Amon_bcc-csm1-1_historical_r1i1p1_185001-201212.nc'   # 65 x 128
+
+varname = 'tasmax'
+
+# step 1: we are looking for the files which will be regridded
+a = get_dst_src_files(file_list=[f1, f3, f4, f5, f2], resolution_type=1)
+
+files_src_grid = a[0] # these files will be regridded (files with source grid)
+files_dst_grid = a[1] # these files will not be regridded (files with destination grid)
+
+
+# step 2: we regrid each file
+
+f_dst = files_dst_grid[0]
+
+regridded_files = []
+
+for f in files_src_grid:
+
+    arr = get_regrided_var(f_src=f, f_dst=f_dst, varname=varname)
+    
+    
+    # Where we will safe the regridded file: for example, in the current directory, and the name of regridded file is the same as source file + "_regridded"
+    
+    result_file_name = basename(f)+'_regridded'
+
+    write2netCDF_after_regridding(arr, f_src=f, f_dst=f_dst, f_out=result_file_name, var_src=varname)
+
+    regridded_files.append(result_file_name)
+        
+    print f, ' regridded !!!'
+    
+
+# step 3: finally we get the list of files with the same grid
+file_list_final = files_dst_grid + regridded_files
+
+print file_list_final
