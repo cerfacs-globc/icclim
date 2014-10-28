986109123eff1be05463389479b65ef7256fce40
Natalia Tatatrinova <natalia.tatarinova@cerfacs.fr>
2014-10-28 16:02:30 +0100

    ...

--- icclim/callback.py
+++ icclim/callback.py
@@ -1,5 +1,9 @@
 # callback called from
 # - icclim.indice(...)
+# - icclim.indice_multivar(...)
+# - icclim.indice_perc(...)
+# - icclim.indice_compound(...)
+# - icclim.get_percentile_dict(...)
 
 
 def defaultCallback(message,percentage):
