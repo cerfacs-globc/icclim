11de68fe0bdf15fb1c6bff930ed68527cf2ae2f8
Christian Page <christian.page@cerfacs.fr>
2016-07-01 13:46:09 +0200

    Updated __init.py__ to 4.2.2.

--- icclim/icclim_doc/source/conf.py
+++ icclim/icclim_doc/source/conf.py
@@ -59,7 +59,7 @@ copyright = u'2015, CERFACS'
 # built documents.
 #
 # The short X.Y version.
-version = '4.2.1'
+version = '4.2.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
