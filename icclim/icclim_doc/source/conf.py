32a423f0499063a5dcf7c7f64cfcf4fa09073aae
Christian Page <christian.page@cerfacs.fr>
2016-07-01 11:48:05 +0200

    Updated to 4.2.1.

--- icclim/icclim_doc/source/conf.py
+++ icclim/icclim_doc/source/conf.py
@@ -59,7 +59,7 @@ copyright = u'2015, CERFACS'
 # built documents.
 #
 # The short X.Y version.
-version = '4.2.0'
+version = '4.2.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
