aa2b6735acdda3049aa095be1061144b9b74eaa3
Xpiv <xavier.pivan.ds@gmail.com>
2019-02-08 12:44:19 +0800

    Add utility function to access data structure from config_indice.json

--- icclim/util/read.py
+++ icclim/util/read.py
@@ -7,3 +7,17 @@ def read_config_file(config_file=config_file):
     with open(config_file) as json_data:
             data = json.load(json_data)
     return data
+
+def get_icclim_indice_config(config_structure=config_structure):
+        #Loading config from icclim for the dispel4py wps workflow
+        return config_structure["icclim"]["indice"].keys()
+
+def get_icclim_slice_mode(config_structure=config_structure):
+        #Loading config from icclim for the dispel4py wps workflow
+        return config_structure["icclim"]["slice_mode"] 
+
+def get_disp4py_config(config_structure=config_structure):
+        #Loading config from icclim for the dispel4py wps workflow
+        conf_filename = config_structure["C4I"]["dispel4py_wps"]["configFileName"]
+        json_structure = config_structure["C4I"]["dispel4py_wps"]["jsonStructure"]
+        return conf_filename, json_structure
