06cdae15aac7edf9899e731a32c411afc3c03ff7
Xpiv <xavier.pivan.ds@gmail.com>
2019-02-11 13:32:37 +0800

    Fix bug in read.py

--- icclim/util/read.py
+++ icclim/util/read.py
@@ -8,16 +8,16 @@ def read_config_file(config_file=config_file):
             data = json.load(json_data)
     return data
 
-def get_icclim_indice_config(config_structure=config_structure):
+def get_icclim_indice_config(config_file=config_file):
         #Loading config from icclim for the dispel4py wps workflow
-        return config_structure["icclim"]["indice"].keys()
+        return config_file["icclim"]["indice"].keys()
 
-def get_icclim_slice_mode(config_structure=config_structure):
+def get_icclim_slice_mode(config_file=config_file):
         #Loading config from icclim for the dispel4py wps workflow
-        return config_structure["icclim"]["slice_mode"] 
+        return config_file["icclim"]["slice_mode"] 
 
-def get_disp4py_config(config_structure=config_structure):
+def get_disp4py_config(config_file=config_file):
         #Loading config from icclim for the dispel4py wps workflow
-        conf_filename = config_structure["C4I"]["dispel4py_wps"]["configFileName"]
-        json_structure = config_structure["C4I"]["dispel4py_wps"]["jsonStructure"]
+        conf_filename = config_file["C4I"]["dispel4py_wps"]["configFileName"]
+        json_structure = config_file["C4I"]["dispel4py_wps"]["jsonStructure"]
         return conf_filename, json_structure
