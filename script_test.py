b4087e1d6e6cedb885171b79a4600315a57105b5
Xpiv <xavier.pivan.ds@gmail.com>
2019-08-15 11:56:23 +0200

    xarray_new_version

--- script_test.py
+++ script_test.py
@@ -21,22 +21,21 @@ def netcdf_processing():
     
     for indice_param in list_indice:
         print('Calulation: '+str(indice_param))
-        if indice_param=='PRCPTOT':
-            if list_indice[indice_param]['indice_type']=='simple':
-                tr = [datetime.datetime(1960,1,1), datetime.datetime(1980,12,31,13)]
+        if list_indice[indice_param]['indice_type']=='simple':
+            tr = [datetime.datetime(1960,1,1), datetime.datetime(1980,12,31,13)]
 
 
-                if 'tas' in list_indice[indice_param]['var_name']:
-                    path_in_file = '/Users/xavier/Projets/data/usecase/tasmax_day_CSIRO-Mk3L-1-2_historical_r1i2p1_18510101_20051231.nc'
-                    icclim.indice(indice_name=indice_param, time_range = tr, base_period_time_range = tr,
-                    in_files=path_in_file, slice_mode='month', var_name='tasmax', ignore_Feb29th=False, 
-                    out_file=out_file, callback=callback.defaultCallback2)
-                else:
-                    path_in_file = "/Users/xavier/Projets/data/test/pr_day_CNRM-CM6-1_highresSST-present_r21i1p1f2_gr_19500101-19991231.nc"
+            if 'tas' in list_indice[indice_param]['var_name']:
+                path_in_file = '/Users/xavier/Projets/data/usecase/tasmax_day_CSIRO-Mk3L-1-2_historical_r1i2p1_18510101_20051231.nc'
+                icclim.indice(indice_name=indice_param, time_range = tr, base_period_time_range = tr,
+                in_files=path_in_file, slice_mode='month', var_name='tasmax', ignore_Feb29th=False, 
+                out_file=out_file, callback=callback.defaultCallback2)
+            else:
+                path_in_file = "/Users/xavier/Projets/data/test/pr_day_CNRM-CM6-1_highresSST-present_r21i1p1f2_gr_19500101-19991231.nc"
 
-                    icclim.indice(indice_name=indice_param, time_range = tr, base_period_time_range = tr,
-                    in_files=path_in_file, slice_mode='month', var_name='pr', ignore_Feb29th=False, 
-                    out_file=out_file, callback=callback.defaultCallback2)
+                icclim.indice(indice_name=indice_param, time_range = tr, base_period_time_range = tr,
+                in_files=path_in_file, slice_mode='month', var_name='pr', ignore_Feb29th=False, 
+                out_file=out_file, callback=callback.defaultCallback2)
 
 if __name__ == "__main__":
     netcdf_processing()
