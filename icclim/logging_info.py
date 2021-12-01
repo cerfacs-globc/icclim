38963228f3b8ed6f561d022616c7cb34256d1365
abel <aoun@cerfacs.fr>
2021-12-01 18:28:52 +0100

    Update logs verbosity

--- icclim/logging_info.py
+++ icclim/logging_info.py
@@ -1,6 +1,9 @@
+# flake8: noqa
+# fmt: off
 import logging
 import os
 import time
+from enum import Enum
 
 import pkg_resources
 
@@ -9,105 +12,52 @@ time.tzset()
 tz = time.tzname[0]
 
 
-def start_message():
+class Verbosity(Enum):
+    LOW = ("LOW",)
+    HIGH = ("HIGH",)
+    NONE = ("NONE",)
+
+
+def start_message(verbosity: Verbosity = Verbosity.HIGH):
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
-    logging.info(
-        "   ********************************************************************************************"
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *          Icclim                V%-10s   *",
-        pkg_resources.get_distribution("icclim").version,
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *          %-28s                                                    *",
-        time.asctime(time.gmtime()) + " " + tz,
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *          BEGIN EXECUTION                                                                 *"
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   ********************************************************************************************"
-    )
+    time_now = time.asctime(time.gmtime()) + " " + tz
+    icclim_version = pkg_resources.get_distribution('icclim').version
+    if verbosity == Verbosity.NONE:
+        return
+    if verbosity == Verbosity.LOW:
+        logging.info(f"Icclim {icclim_version} --  {time_now}")
+        logging.info("--- BEGIN EXECUTION ---")
+        return
+    logging.info("   ********************************************************************************************")
+    logging.info("   *                                                                                          *")
+    logging.info(f"   *          Icclim                {icclim_version}   *")
+    logging.info("   *                                                                                          *")
+    logging.info("   *                                                                                          *")
+    logging.info(f"   *          {time_now}                                                    *")
+    logging.info("   *                                                                                          *")
+    logging.info("   *          BEGIN EXECUTION                                                                 *")
+    logging.info("   *                                                                                          *")
+    logging.info("   ********************************************************************************************")
 
-
-def wrong_variable_attribute_message(in_valid):
-    # TODO see if we still want to use this
-    if in_valid:
-        logging.warning(
-            "   ********************************************************************************************"
-        )
-        logging.warning(
-            "   *                                                                                          *"
-        )
-        logging.warning(
-            '   *          CANNOT HANDLE variable attributes "valid_min", "valid_max", and "valid_range"   *'
-        )
-        logging.warning(
-            "   *                        values beyond these limits will be included in the computations   *"
-        )
-        logging.warning(
-            '   *                        If present "missing_value" and/or "_FillValue" will be honoured   *'
-        )
-        logging.warning(
-            "   *                                                                                          *"
-        )
-        logging.warning(
-            "   ********************************************************************************************"
-        )
-
-
-def ending_message(time_cpu, tz=tz):
-    logging.info(
-        "   ********************************************************************************************"
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *          Icclim                V%-10s   *",
-        pkg_resources.get_distribution("icclim").version,
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *          %-28s                                                    *",
-        time.asctime(time.gmtime()) + " " + tz,
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *          END EXECUTION                                                                   *"
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   *          CP SECS = %-10.3f                                                            *",
-        time_cpu,
-    )
-    logging.info(
-        "   *                                                                                          *"
-    )
-    logging.info(
-        "   ********************************************************************************************"
-    )
+def ending_message(time_cpu, tz=tz, verbosity: Verbosity = Verbosity.HIGH):
+    time_now = time.asctime(time.gmtime()) + " " + tz
+    icclim_version = pkg_resources.get_distribution('icclim').version
+    if verbosity == Verbosity.NONE:
+        return
+    if verbosity == Verbosity.LOW:
+        logging.info(f"Icclim {icclim_version} --  {time_now}")
+        logging.info("CPU SECS = %-10.3f", time_cpu)
+        logging.info("--- END EXECUTION ---")
+        return
+    logging.info("   ********************************************************************************************")
+    logging.info("   *                                                                                          *")
+    logging.info(f"   *          Icclim                {icclim_version}   *")
+    logging.info("   *                                                                                          *")
+    logging.info("   *                                                                                          *")
+    logging.info(f"   *          {time_now}                                                    *")
+    logging.info("   *                                                                                          *")
+    logging.info("   *          END EXECUTION                                                                   *")
+    logging.info("   *                                                                                          *")
+    logging.info(f"   *          CP SECS = {time_cpu}                                                            *")
+    logging.info("   *                                                                                          *")
+    logging.info("   ********************************************************************************************")
