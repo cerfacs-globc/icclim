48f3eff732bdc0e89f8e6342c592c05555b79292
abel <aoun@cerfacs.fr>
2022-01-03 10:03:37 +0100

    Fix: improper lint on doc conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,9 +13,8 @@
 import os
 import sys
 
-import icclim
-
 sys.path.insert(0, os.path.abspath("../.."))
+import icclim
 
 # -- Project information -----------------------------------------------------
 project = "Icclim"
