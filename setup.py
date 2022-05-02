1233aecf26fb67da8ff04768f9896c63a657a97b
abel <aoun@cerfacs.fr>
2022-05-02 11:04:08 +0200

    FIX: rechunker pinning

--- setup.py
+++ setup.py
@@ -10,16 +10,12 @@ MINIMAL_REQUIREMENTS = [
     "xarray>=0.17",
     "xclim>=0.34",
     "cftime>=1.4.1",
-    # todo: unpin dask once we can move to rechunker 0.4
-    #       https://github.com/pangeo-data/rechunker/issues/110
-    "dask[array]<2022.01.1",
+    "dask[array]",
     "netCDF4>=1.5.7",
     "pyyaml",
     "psutil",
     "zarr",
-    # todo: unpin rechunker once
-    #       https://github.com/pangeo-data/rechunker/issues/92 is fixed
-    "rechunker>=0.3,<0.4",
+    "rechunker>=0.3, !=0.4",
     "fsspec",
 ]
 
