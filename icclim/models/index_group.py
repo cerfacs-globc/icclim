6b807ff610b6c45eafe083595e245e016393921d
abel <aoun@cerfacs.fr>
2022-10-03 17:03:30 +0200

    MAINT: Add union of IndexGroup using __or__
    The __or__ operator on IndexGroup now creates a
     new IndexGroup instance bounded to the two
     unionised groups.
    This make the former "COMPOUND" group useless
     as composition is not unrestricted.

--- icclim/models/index_group.py
+++ icclim/models/index_group.py
@@ -6,15 +6,39 @@ from typing import Any
 from icclim.models.registry import Registry
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(init=False)
 class IndexGroup:
     name: str
+    values: list[IndexGroup]
+
+    def __init__(self, name: str, values: list[IndexGroup] = None):
+        self.name = name
+        if values is None:
+            self.values = [self]
+        else:
+            self.values = values
 
     def get_indices(self) -> list[Any]:
         # import locally to avoid circular dependency (an index has a IndexGroup)
         from icclim.ecad.ecad_indices import EcadIndexRegistry
 
-        return list(filter(lambda i: i.group == self, EcadIndexRegistry.values()))
+        return list(
+            filter(lambda i: i.group in self.values, EcadIndexRegistry.values())
+        )
+
+    def __or__(self, right):
+        """Used to compose IndexGroup, e.g., IndexGroup1 | IndexGroup2."""
+        if isinstance(right, IndexGroup):
+            return IndexGroup(f"{self.name}_{right.name}", [self, right])
+        raise NotImplementedError(
+            f"Unexpected type for {right}: {type(right)}."
+            f" An IndexGroup was expected."
+        )
+
+    def __eq__(self, other):
+        if not isinstance(other, IndexGroup):
+            return False
+        return other.name == self.name
 
 
 class IndexGroupRegistry(Registry):
@@ -26,7 +50,5 @@ class IndexGroupRegistry(Registry):
     DROUGHT = IndexGroup("drought")
     RAIN = IndexGroup("rain")
     SNOW = IndexGroup("snow")
-    COMPOUND = IndexGroup("compound")
-    GENERIC = IndexGroup("generic")
     # no climate index should be bounded to "all"
     WILD_CARD_GROUP = IndexGroup("all")
