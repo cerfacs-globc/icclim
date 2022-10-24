0c508ba7a7c498c408d6e78bddb1cf2230116dd1
abel <aoun@cerfacs.fr>
2022-10-24 17:56:02 +0200

    DOC: Add definitions to Generic indices

--- icclim/generic_indices/generic_indicators.py
+++ icclim/generic_indices/generic_indicators.py
@@ -184,6 +184,7 @@ class GenericIndicator(ResamplingIndicator):
         self,
         name: str,
         process: Callable[..., DataArray],
+        definition: str,
         check_vars: (
             Callable[[list[ClimateVariable], GenericIndicator], None] | None
         ) = None,
@@ -198,6 +199,7 @@ class GenericIndicator(ResamplingIndicator):
         self.cell_methods = local[name]["cell_methods"]
         self.long_name = local[name]["long_name"]
         self.check_vars = check_vars
+        self.definition = definition
         self.sampling_methods = (
             sampling_methods if sampling_methods is not None else [RESAMPLE_METHOD]
         )
@@ -731,49 +733,131 @@ class GenericIndicatorRegistry(Registry[GenericIndicator]):
 
     _item_class = GenericIndicator
 
-    CountOccurrences = GenericIndicator("count_occurrences", count_occurrences)
+    CountOccurrences = GenericIndicator(
+        "count_occurrences",
+        count_occurrences,
+        definition="Count occurrences where threshold(s) are met"
+        " (e.g. SU, Tx90p, RR1).",
+    )
     MaxConsecutiveOccurrence = GenericIndicator(
         "max_consecutive_occurrence",
         max_consecutive_occurrence,
+        definition="Count the maximum number of consecutive occurrences when"
+        " threshold(s) are met (e.g. CDD, CSU, CWD).",
     )
     SumOfSpellLengths = GenericIndicator(
         "sum_of_spell_lengths",
         sum_of_spell_lengths,
+        definition="Sum the lengths of each consecutive occurrence spell when"
+        " threshold(s) are met. The minimum spell length is controlled by"
+        " `min_spell_length` (e.g. WSDI, CSDI).",
     )
-    Excess = GenericIndicator("excess", excess, check_vars=_check_single_var)
-    Deficit = GenericIndicator("deficit", deficit, check_vars=_check_single_var)
+    Excess = GenericIndicator(
+        "excess",
+        excess,
+        check_vars=_check_single_var,
+        definition="Compute the excess over the given threshold. The excess is"
+        " `sum(x[x>t] - t)` where x is the studied variable and t the threshold"
+        " (e.g. GD4).",
+    )
+    Deficit = GenericIndicator(
+        "deficit",
+        deficit,
+        check_vars=_check_single_var,
+        definition="Compute the deficit below the given threshold. The deficit is"
+        " `sum(t - x[x<t])` where x is the studied variable and t the threshold"
+        " (e.g. HD17).",
+    )
     FractionOfTotal = GenericIndicator(
-        "fraction_of_total", fraction_of_total, check_vars=_check_single_var
+        "fraction_of_total",
+        fraction_of_total,
+        check_vars=_check_single_var,
+        definition="Compute the fraction of values meeting threshold(s) over the sum of"
+        " every values (e.g. R75pTOT, R95pTOT).",
     )
-    Maximum = GenericIndicator("maximum", maximum)
-    Minimum = GenericIndicator("minimum", minimum)
-    Average = GenericIndicator("average", average)
-    Sum = GenericIndicator("sum", sum)
-    StandardDeviation = GenericIndicator("standard_deviation", standard_deviation)
+    Maximum = GenericIndicator(
+        "maximum",
+        maximum,
+        definition="Maximum of values that met threshold(s), if threshold(s) are given"
+        " (e.g. Txx, Tnx).",
+    )
+    Minimum = GenericIndicator(
+        "minimum",
+        minimum,
+        definition="Maximum of values that met threshold(s), if threshold(s) are given"
+        " (e.g. Txn, Tnn).",
+    )
+    Average = GenericIndicator(
+        "average",
+        average,
+        definition="Average of values that met threshold(s), if threshold(s) are given"
+        " (e.g. Tx, Tn)",
+    )
+    Sum = GenericIndicator(
+        "sum",
+        sum,
+        definition="Sum of values that met threshold(s), if threshold(s) are given"
+        " (e.g. PRCPTOT, RR).",
+    )
+    StandardDeviation = GenericIndicator(
+        "standard_deviation",
+        standard_deviation,
+        definition="Standard deviation of values that met threshold(s),"
+        " if threshold(s) are given.",
+    )
     MaxOfRollingSum = GenericIndicator(
-        "max_of_rolling_sum", max_of_rolling_sum, check_vars=_check_single_var
+        "max_of_rolling_sum",
+        max_of_rolling_sum,
+        check_vars=_check_single_var,
+        definition="Maximum of rolling sum over time dimension"
+        " (e.g. RX5DAY: maximum 5 days window of precipitation accumulation).",
     )
     MinOfRollingSum = GenericIndicator(
-        "min_of_rolling_sum", min_of_rolling_sum, check_vars=_check_single_var
+        "min_of_rolling_sum",
+        min_of_rolling_sum,
+        check_vars=_check_single_var,
+        definition="Minimum of rolling sum over time dimension.",
     )
     MaxOfRollingAverage = GenericIndicator(
-        "max_of_rolling_average", max_of_rolling_average, check_vars=_check_single_var
+        "max_of_rolling_average",
+        max_of_rolling_average,
+        check_vars=_check_single_var,
+        definition="Maximum of rolling average over time dimension.",
     )
     MinOfRollingAverage = GenericIndicator(
-        "min_of_rolling_average", min_of_rolling_average, check_vars=_check_single_var
+        "min_of_rolling_average",
+        min_of_rolling_average,
+        check_vars=_check_single_var,
+        definition="Minimum of rolling average over time dimension.",
     )
     MeanOfDifference = GenericIndicator(
-        "mean_of_difference", mean_of_difference, check_vars=_check_couple_of_vars
+        "mean_of_difference",
+        mean_of_difference,
+        check_vars=_check_couple_of_vars,
+        definition="Average of the difference between two variables"
+        ", or one variable and it's reference period values"
+        " (e.g. DTR: `mean(tasmax - tasmin)`).",
     )
     DifferenceOfExtremes = GenericIndicator(
         "difference_of_extremes",
         difference_of_extremes,
         check_vars=_check_couple_of_vars,
+        definition="Difference of extremes between two variables"
+        ", or one variable and it's reference period values."
+        " The extremes are always `maximum` for the first variable and"
+        " `minimum` for the second variable"
+        " (e.g. ETR: `max(tasmax) - min(tasmin)`).",
     )
     MeanOfAbsoluteOneTimeStepDifference = GenericIndicator(
         "mean_of_absolute_one_time_step_difference",
         mean_of_absolute_one_time_step_difference,
         check_vars=_check_couple_of_vars,
+        definition="Average of the absolute one time step by one time step difference"
+        " between two variables,"
+        " or one variable and it's reference period values"
+        " (e.g. vDTR:"
+        " `mean((tasmax[i] - tasmin[i]) - (tasmax[i-1] - tasmin[i-1])` ;"
+        " where i is the day of measure).",
     )
     DifferenceOfMeans = GenericIndicator(
         "difference_of_means",
@@ -784,6 +868,9 @@ class GenericIndicatorRegistry(Registry[GenericIndicator]):
             GROUP_BY_METHOD,
             GROUP_BY_REF_AND_RESAMPLE_STUDY_METHOD,
         ],
+        definition="Difference of the average between two variables"
+        ", or one variable and it's reference period values"
+        " (e.g. anomaly: `mean(tasmax) - mean(tasmax_ref]))`.",
     )
 
 
