FIX: [#256] RxxpTOT computation
The unit of the thresolf min value is now converted before computation.

icclim/generic_indices/generic_indicators.py
@@ -412,8 +412,11 @@ def fraction_of_total(
 ) -> DataArray:
     study, threshold = get_single_var(climate_vars)
     if threshold.threshold_min_value is not None:
+        min_val = threshold.threshold_min_value
+        min_val = convert_units_to(min_val, study, context="hydro")
         total = (
-            study.where(threshold.operator(study, threshold.threshold_min_value.m))
+            study.where(threshold.operator(study, min_val))
+            # study.where(threshold.operator(study, threshold.threshold_min_value.m))
             .resample(time=resample_freq.pandas_freq)
             .sum(dim="time")
         )

icclim/tests/test_main.py
@@ -508,9 +508,9 @@ class Test_Integration:
         np.testing.assert_array_almost_equal(res.PRCPTOT.isel(time=0), 42)
 
     def test_index_r75ptot(self):
-        pr = stub_pr(value=0.00002)
+        pr = stub_pr(value=2.32e-05) # about 2 mm/day
         pr.attrs["units"] = "kg m-2 s-1"
-        pr[:10] = 100
+        pr[:10] = 2e-03
         res = icclim.r75ptot(
             in_files=pr,
             out_file=self.OUTPUT_FILE,