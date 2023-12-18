2920ac3a911cd3cf13347803f7efb20ef58cc8c8
Abel Aoun <aoun.abel@outlook.com>
2023-12-18 14:48:46 +0100

    MAINT: Update CI

--- tools/build.sh
+++ tools/build.sh
@@ -2,6 +2,6 @@
 # from icclim root
 # and with "icclim-dev" conda environment activated
 python -m setup install
-python ./tools/extract-icclim-funs.py ./icclim/_generated_api.py
-git add ./icclim/_generated_api.py
+python ./tools/extract-icclim-funs.py ./src/icclim/_generated_api.py
+git add ./src/icclim/_generated_api.py
 pre-commit run
